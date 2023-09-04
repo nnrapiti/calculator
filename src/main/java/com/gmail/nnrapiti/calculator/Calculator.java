63bdcc6ce891e28ac2590e55bda3749bf63abc62
nnrapiti <nnrapiti@gmail.com>
2023-09-04 11:54:43 +0200

    Initial Commit - Set up maven project - main class and method - one test to run main method

---
+++ src/main/java/com/gmail/nnrapiti/calculator/Calculator.java
@@ -0,0 +1,7 @@
+package com.gmail.nnrapiti.calculator;
+
+public class Calculator {
+    public static void main(String[] args) {
+        System.out.println("hello World");
+    }
+}
---
+++ src/test/java/com/gmail/nnrapiti/calculator/CalculatorTest.java
@@ -0,0 +1,10 @@
+package com.gmail.nnrapiti.calculator;
+
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+    @Test
+    public void firstTest(){
+        Calculator.main(new String[0]);
+    }
+}
