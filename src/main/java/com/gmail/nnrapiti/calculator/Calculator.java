46078527cd5c8288a21aef03c267950f5be675e8
nnrapiti <nnrapiti@gmail.com>
2023-09-04 19:15:24 +0200

    Core Pieces  - Add handling for number 1, operator, number 2 in order  - Currently Only Adds numbers together

--- src/main/java/com/gmail/nnrapiti/calculator/Calculator.java
+++ src/main/java/com/gmail/nnrapiti/calculator/Calculator.java
@@ -1,7 +1,58 @@
 package com.gmail.nnrapiti.calculator;
 
 public class Calculator {
-    public static void main(String[] args) {
-        System.out.println("hello World");
+    public static void main(String[] args){
+        int x = getXFromArgs(args);
+        int y = getYFromArgs(args);
+        System.out.println(addNumbers(x, y));
     }
+
+    public static int addNumbers(int x, int y){
+        return x + y;
+    }
+
+    public static int getXFromArgs(String[] args){
+        return getNumberFromStringArray(args);
+    }
+    public static String getOperatorFromArgs(String[] args){
+        for(String arg: args){
+            try{
+                Integer.parseInt(arg);
+            }catch (NumberFormatException e){
+                return arg;
+            }
+        }
+        return "ADD";
+    }
+
+    public static int getYFromArgs(String[] args){
+        int index = 0;
+        for(String arg: args){
+            try{
+                Integer.parseInt(arg);
+            }catch (NumberFormatException e){
+                index++;
+                break;
+            }
+            index++;
+        }
+        String[] argsYSubset = new String[args.length-index];
+        System.arraycopy(args, index, argsYSubset, 0, args.length-index);
+        return getNumberFromStringArray(argsYSubset);
+    }
+
+    public static int getNumberFromStringArray(String[] numbers){
+        StringBuilder fullNumber = new StringBuilder();
+        for(String number: numbers){
+            try{
+                Integer.parseInt(number);
+            }catch (NumberFormatException e){
+                break;
+            }
+            fullNumber.append(number);
+        }
+        return Integer.parseInt(fullNumber.toString());
+    }
+
+
 }
--- src/test/java/com/gmail/nnrapiti/calculator/CalculatorTest.java
+++ src/test/java/com/gmail/nnrapiti/calculator/CalculatorTest.java
@@ -2,9 +2,98 @@ package com.gmail.nnrapiti.calculator;
 
 import org.junit.jupiter.api.Test;
 
+import static com.gmail.nnrapiti.calculator.Calculator.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CalculatorTest {
+
     @Test
-    public void firstTest(){
-        Calculator.main(new String[0]);
+    public void additionFunction(){
+        int result = addNumbers(5, 11);
+        assertEquals(16, result);
     }
+    @Test
+    public void getFirstNumberSingleDigit(){
+        String[] args = new String[]{"5"};
+        assertEquals(5, getXFromArgs(args));
+    }
+    @Test
+    public void getFirstNumberMultipleDigitSameSpace(){
+        String[] args = new String[]{"38"};
+        assertEquals(38, getXFromArgs(args));
+    }
+
+    @Test
+    public void getFirstNumberMultipleDigitDifferentSpace(){
+        String[] args = new String[]{"3", "8"};
+        assertEquals(38, getXFromArgs(args));
+    }
+    @Test
+    public void getFirstNumberMultipleDigitDifferentSpaceQuiteAFewNumbers(){
+        String[] args = new String[]{"3", "8", "9", "7", "4", "2", "3"};
+        assertEquals(3897423, getXFromArgs(args));
+    }
+
+    @Test
+    public void getFirstNumberSingleDigitWithOperator(){
+        String[] args = new String[]{"5", "ADD"};
+        assertEquals(5, getXFromArgs(args));
+    }
+
+    @Test
+    public void getFirstNumberMultipleDigitSameSpaceWithOperator(){
+        String[] args = new String[]{"38", "ADD"};
+        assertEquals(38, getXFromArgs(args));
+    }
+
+    @Test
+    public void getFirstNumberMultipleDigitDifferentSpaceWithOperator(){
+        String[] args = new String[]{"3", "8", "ADD"};
+        assertEquals(38, getXFromArgs(args));
+    }
+
+    @Test
+    public void getOperatorADD(){
+        String[] args = new String[]{"5", "ADD"};
+        assertEquals("ADD", getOperatorFromArgs(args));
+    }
+
+    @Test
+    public void getOperatorMINUS(){
+        String[] args = new String[]{"5", "MINUS"};
+        assertEquals("MINUS", getOperatorFromArgs(args));
+    }
+
+    @Test
+    public void getSecondNumberSingleDigit(){
+        String[] args = new String[]{"5", "ADD", "3"};
+        assertEquals(3, getYFromArgs(args));
+    }
+    @Test
+    public void getSecondNumberMultipleDigitSameSpace(){
+        String[] args = new String[]{"38", "MINUS", "65"};
+        assertEquals(65, getYFromArgs(args));
+    }
+
+    @Test
+    public void getSecondNumberMultipleDigitDifferentSpace(){
+        String[] args = new String[]{"3", "8", "ADD", "5", "6"};
+        assertEquals(56, getYFromArgs(args));
+    }
+    @Test
+    public void getSecondNumberMultipleDigitDifferentSpaceQuiteAFewNumbers(){
+        String[] args = new String[]{"3", "8", "9", "7", "4", "2", "3", "ADD", "2", "3", "9", "7", "9", "7"};
+        assertEquals(239797, getYFromArgs(args));
+    }
+
+    @Test
+    public void addNumbersUsingStringArray(){
+        String[] args = new String[]{"6", "ADD", "5"};
+        int x = getXFromArgs(args);
+        int y = getYFromArgs(args);
+        int result = addNumbers(x, y);
+        assertEquals(11, result);
+
+    }
+
 }
