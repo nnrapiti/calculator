6c65438906a439b4de99a933126e211fb2770ff4
nnrapiti <nnrapiti@gmail.com>
2023-09-05 15:09:46 +0200

    Changed user interface to prompt for inputs

--- src/main/java/com/gmail/nnrapiti/calculator/Calculator.java
+++ src/main/java/com/gmail/nnrapiti/calculator/Calculator.java
@@ -1,30 +1,65 @@
 package com.gmail.nnrapiti.calculator;
 
-public class Calculator {
-    public static void main(String[] args){
-        int x = getXFromArgs(args);
-        int y = getYFromArgs(args);
-        String operation = getOperatorFromArgs(args);
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
-        System.out.println(doOperation(x, operation, y));
+public class Calculator {
+    public static void main(String[] args) {
+
+        String[] arguments = getInputFromUser().toArray(new String[0]);
+
+        int x = getXFromArgs(arguments);
+        int y = getYFromArgs(arguments);
+        String operation = getOperatorFromArgs(arguments);
+
+        System.out.println("Result = " + doOperation(x, operation, y));
+    }
+
+    private static List<String> getInputFromUser() {
+        List<String> arguments = new ArrayList<>();
+
+        Scanner scanner = new Scanner(System.in);
+        String operation = "UNKNOWN";
+
+        System.out.print("Please enter First Number: ");
+        arguments.add(String.valueOf(scanner.nextInt()));
+        System.out.println();
+        while (operation.equalsIgnoreCase("UNKNOWN")) {
+            System.out.println();
+            System.out.print("Please enter Operation:");
+            int op = scanner.nextInt();
+            operation = switch (op){
+                case 1 -> "ADD";
+                case 2 -> "SUBTRACT";
+                case 3 -> "MULTIPLY";
+                case 4 -> "DIVIDE";
+                default -> "UNKNOWN";
+            };
+            System.out.println("OPERATION SELECTED: " + operation);
+        }
+        arguments.add(operation);
+
+        System.out.println();
+        System.out.print("Please enter Second Number: ");
+        arguments.add(String.valueOf(scanner.nextInt()));
+
+        scanner.close();
+        return arguments;
     }
 
     public static int addNumbers(int x, int y){
         return x + y;
     }
-
     public static int subtractNumbers(int x, int y){
         return x - y;
     }
-
     public static int multiplyNumbers(int x, int y){
         return x * y;
     }
     public static int divideNumbers(int x, int y){
         return x / y;
     }
-
-
 
     public static int getXFromArgs(String[] args){
         return getNumberFromStringArray(args);
@@ -70,7 +105,6 @@ public class Calculator {
     }
 
     public static int doOperation(int x, String operation, int y){
-
         return switch (operation){
             case "ADD" -> addNumbers(x, y);
             case "SUBTRACT" -> subtractNumbers(x, y);
